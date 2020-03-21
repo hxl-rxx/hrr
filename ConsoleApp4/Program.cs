c0de52d30752da2f07c85352a8d59de6f1882e65
1720934884@qq.com <1720934884@qq.com>
2020-03-21 16:12:09 +0800

    woderen

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp4
         static void Main(string[] args)
         {
             Console.WriteLine("贺小龙是1708A的");
-
+            Console.WriteLine("贺小龙是1708B的");
         }
     }
 }
